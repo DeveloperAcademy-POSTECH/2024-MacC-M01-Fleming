f28cee15ff0f5066b17e5031f0eb1a9f222f868b
Bright-jeans <Bright.jjeans@gmail.com>
2024-10-08 12:47:28 +0900

    [#3]Feat, UI연습,카메라배경에 png애니메이션

--- Fleming_App/Fleming_App/Model_Test/MonitorSound_Trial/CameraView.swift
+++ Fleming_App/Fleming_App/Model_Test/MonitorSound_Trial/CameraView.swift
@@ -25,6 +25,7 @@ struct CameraView: UIViewControllerRepresentable {
 
 class CameraViewController: UIViewController {
     var viewModel: CameraViewModel?
+    var previewLayer: AVCaptureVideoPreviewLayer? // 뷰 레이아웃 변경용
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,9 +37,16 @@ class CameraViewController: UIViewController {
         guard let captureSession = viewModel?.captureSession else { return }
         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.frame = view.bounds
-        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill // 정확한 타입 지정
+        previewLayer.videoGravity = .resizeAspect // 화면에 맞추되 비율 유지하며 여백 추가
         view.layer.addSublayer(previewLayer)
+        self.previewLayer = previewLayer
     }
+    
+    // 뷰의 레이아웃이 변경될 때마다 호출되어 레이어 크기를 조정
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+            previewLayer?.frame = view.bounds // 화면의 전체 좌우폭에 맞게 크기 설정
+        }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
