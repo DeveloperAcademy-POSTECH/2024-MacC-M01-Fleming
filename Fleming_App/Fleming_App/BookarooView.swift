39ec5e8f19142caf40d66cfe884d2343230ddb5f
dodo <rlaehgus0909@gmail.com>
2024-10-16 14:36:35 +0900

    [#23]UI수정-DoDo

--- Fleming_App/Fleming_App/BookarooView.swift
+++ Fleming_App/Fleming_App/BookarooView.swift
@@ -1,3 +1,4 @@
+
 //
 //  BookarooView.swift
 //  Fleming_App
@@ -14,54 +15,75 @@ struct BookarooView: View{
     var screenWidth = UIScreen.main.bounds.width
     var screenHeight = UIScreen.main.bounds.height
     
-    var body: some View{
-        
-        VStack{
-            Text("Hi Leo")
-                .font(.system(size: 128))
-                .bold()
-                .frame(width: screenWidth-80, alignment: .leading)
-                .foregroundStyle(AppColor.pigBrown)
-            
-            Spacer()
-            
-            ScrollView(.horizontal){
-                LazyHStack(spacing: 60) {
-                    // 네비게이션 한 개
-                    
-                    NavigationLink(destination: ThreeLittlePigsNavigation(currentStep: $currentStep, isLeft:$isLeft)){
-                        Image("Cover_pig")
-                            .resizable()
-                            .frame(width: 0.23 * screenWidth, height: 0.50 * screenHeight)
-                            .aspectRatio(contentMode: .fit)
-                            .clipShape(RoundedRectangle(cornerRadius: 32))
-                            .contentShape(Rectangle())
+    var body: some View {
+        NavigationView{
+            ZStack {
+                VStack {
+                    ZStack {
+                        HStack{
+                            Spacer()
+                            NavigationLink(destination: GuardianView()) {
+                                Image(systemName: "person.circle.fill")
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                    .foregroundColor(.black)
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                            .padding(.leading, 20)
+                        }
+                        HStack(spacing: 0) {
+                            Text("Book")
+                                .font(.system(size: 40, weight: .bold))
+                            Text("aroo")
+                                .font(.system(size: 40))
+                        }
+                        .frame(maxWidth: .infinity)
+                        .multilineTextAlignment(.center)
+                        
+                        Spacer()
+                        
                     }
-                    
-                    // 뒤에 추가하는 것들... 컨텐츠에 따라 컴포넌트화 할 예정...
-                    ForEach(0..<10) { index in
-                        VStack(alignment:.leading) {
-                            Button(action: {
-                                print("\(index+1)번 동화 클릭")
-                            },label:{Rectangle()
-                                    .fill(Color.gray)
-                                    .frame(width: 0.23 * screenWidth, height: 0.50 * screenHeight)
-                                    .overlay(
-                                        Text("\(index+1)번 동화")
-                                            .font(.title)
-                                            .foregroundStyle(.white)
-                                    )
-                                    .clipShape(RoundedRectangle(cornerRadius: 32))
-                            })
-                        } //
-                    } // ForEach 끝
+                    .frame(height: 50)
+                    .padding(.horizontal, 20)
+                    .padding(.top, 10)
+                    Spacer()
+                    ScrollView(.horizontal){
+                        HStack {
+                            NavigationLink(destination: ThreeLittlePigsNavigation(currentStep: $currentStep, isLeft: $isLeft)){
+                                Image("Cover_pigs")
+                                    .resizable()
+                                    .frame(width: 290, height: 400)
+                                    .shadow(color: Color.black.opacity(0.25), radius: 20, x: 5, y: 5)
+                                    .padding(.leading, 30)
+                            }
+                            Image("Cover_rsp")
+                                .resizable()
+                                .frame(width: 290, height: 400)
+                                .shadow(color: Color.black.opacity(0.25), radius: 20, x: 5, y: 5)
+                                .padding(.leading, 30)
+                            Image("Cover_dots")
+                                .resizable()
+                                .frame(width: 290, height: 400)
+                                .shadow(color: Color.black.opacity(0.25), radius: 20, x: 5, y: 5)
+                                .padding(.leading, 30)
+                            
+                            
+                            
+                        }
+                        .padding(50)
+                    }
+                    Spacer()
                 }
+                .frame(height: screenHeight * 0.85)
             }
-            .padding(.horizontal, 5)
-            .padding(.top, -40)
-            
-            Spacer()
         }
-        .frame(height: screenHeight * 0.85)
+        .navigationViewStyle(StackNavigationViewStyle()) // iPad에서도 스택 네비게이션 강제
     }
 }
+
+#Preview {
+    @Previewable @State var isLeft: Bool = false
+    @Previewable @State var currentStep: Int = 1
+    BookarooView(currentStep: $currentStep, isLeft: $isLeft)
+}
+
