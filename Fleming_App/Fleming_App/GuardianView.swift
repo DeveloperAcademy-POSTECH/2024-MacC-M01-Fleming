abc50455b6aae56bbdcf5877b8fdebe66d3fdbf0
dodo <rlaehgus0909@gmail.com>
2024-10-16 16:40:34 +0900

    가디언뷰 수정

--- Fleming_App/Fleming_App/GuardianView.swift
+++ Fleming_App/Fleming_App/GuardianView.swift
@@ -1,114 +1,99 @@
 import SwiftUI
 
 struct GuardianView: View {
-    @State private var toggleCategory1: Bool = true
-    @State private var toggleCategory2: Bool = true
-    @State private var toggleCategory3: Bool = true
-    @State private var toggleCategory4: Bool = true
-    @State private var toggleCategory5: Bool = true
+    @State private var name: String = "John Doe"
+    @State private var age: Date = Date()
+    @State private var gender: String = "Male"
+    @State private var isEditing: Bool = false
+    @State private var physician: String = "Dr. Smith"
+    @State private var note: String = "Regular check-up"
     
     var screenWidth = UIScreen.main.bounds.width
     var screenHeight = UIScreen.main.bounds.height
     
-    var body: some View { 
+    var body: some View {
         NavigationView {
             VStack {
-                HStack {
-                    // 뒤로가기 버튼 추가
-                    Button(action: {
-                        // 뒤로가기 버튼의 동작 설정
-                        // navigation stack에서 pop(이전 화면으로 돌아가기)
-                    }) {
-                        HStack {
-                            Image(systemName: "arrow.left") // 뒤로가기 화살표 아이콘
-                                .font(.title)
-                        }
-                        .foregroundColor(.black)
-                    }
-                    .padding(.leading, 20) // 왼쪽에 여백 추가
-                    
-                    Spacer() // 왼쪽 정렬 유지
-                }
-                .padding(.top, 10) // 상단에 약간의 여백 추가
+                Text(name)
+                    .font(.system(size: 128))
+                    .bold()
+                    .padding(.leading, 40)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 
-                // 기존 GuardianView의 내용들
-                HStack(alignment: .bottom, spacing: 40){
-                    Text("Hi Leo")
-                        .font(.system(size: 128))
-                        .bold()
-                        .foregroundStyle(AppColor.pigBrown)
-                    
-                    Text("8, Boy")
+                HStack {
+                    Text("Birth:")
                         .font(.system(size: 40))
                         .bold()
-                        .foregroundStyle(AppColor.pigBrown)
+                    Text(age, style: .date)
+                        .font(.system(size: 40))
+                        .bold()
                 }
                 .frame(width: screenWidth-80, alignment: .leading)
                 
-                Spacer()
                 HStack {
                     VStack(alignment: .leading) {
-                        Text("Attending physician :")
-                            .font(.system(size: 40))
-                            .bold()
-                            .foregroundStyle(AppColor.pigBrown)
-                        Text("Note :")
-                            .font(.system(size: 40))
-                            .bold()
-                            .foregroundStyle(AppColor.pigBrown)
-                        Text("Mode :")
-                            .font(.system(size: 40))
-                            .bold()
-                            .foregroundStyle(AppColor.pigBrown)
+                        HStack {
+                            Spacer().frame(width: 40)
+                            Text("Attending physician :")
+                                .font(.system(size: 40))
+                                .bold()
+                            Text(physician)
+                                .font(.system(size: 40))
+                        }
+                        HStack {
+                            Spacer().frame(width: 40)
+                            Text("Note :")
+                                .font(.system(size: 40))
+                                .bold()
+                            Text(note)
+                                .font(.system(size: 40))
+                        }
                     }
                     Spacer()
                 }
                 
                 Spacer()
-                
-                HStack {
-                    Toggle("Physic", isOn: $toggleCategory1)
-                        .font(.system(size: 40))
-                        .bold()
-                        .foregroundStyle(AppColor.pigBrown)
-                        .frame(width: screenWidth * 0.25)
-                    
-                    Toggle("Voice", isOn: $toggleCategory2)
-                        .font(.system(size: 40))
-                        .bold()
-                        .foregroundStyle(AppColor.pigBrown)
-                        .frame(width: screenWidth * 0.25)
-                    
-                    Toggle("Recognition", isOn: $toggleCategory3)
-                        .font(.system(size: 40))
-                        .bold()
-                        .foregroundStyle(AppColor.pigBrown)
-                        .frame(width: screenWidth * 0.25)
-                }
-                
-                Spacer()
-                
-                HStack {
-                    Toggle("Concentrate", isOn: $toggleCategory4)
-                        .font(.system(size: 40))
-                        .bold()
-                        .foregroundStyle(AppColor.pigBrown)
-                        .frame(width: screenWidth * 0.25)
-                    
-                    Toggle("Voice", isOn: $toggleCategory5)
-                        .font(.system(size: 40))
-                        .bold()
-                        .foregroundStyle(AppColor.pigBrown)
-                        .frame(width: screenWidth * 0.25)
-                    
-                    Text(" ")
-                        .frame(width: screenWidth * 0.25)
-                }
-                Spacer()
             }
             .frame(height: screenHeight * 0.85)
+            .navigationBarItems(trailing: Button("Edit") {
+                isEditing = true
+            })
+            .sheet(isPresented: $isEditing) {
+                EditGuardianView(name: $name, age: $age, gender: $gender, physician: $physician, note: $note)
+            }
         }
         .navigationViewStyle(StackNavigationViewStyle()) // iPad에서도 스택 네비게이션 강제
+    }
+}
+
+struct EditGuardianView: View {
+    @Binding var name: String
+    @Binding var age: Date
+    @Binding var gender: String
+    @Binding var physician: String
+    @Binding var note: String
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("User Info")) {
+                    TextField("Name", text: $name)
+                    DatePicker("Birthday", selection: $age, displayedComponents: .date)
+                    TextField("Gender", text: $gender)
+                    TextField("Attending Physician", text: $physician)
+                    TextField("Note", text: $note)
+                }
+            }
+            .navigationBarTitle("Edit Info", displayMode: .inline)
+            .navigationBarItems(leading: Button("") {
+                presentationMode.wrappedValue.dismiss()
+            }, trailing: Button("Save") {
+                presentationMode.wrappedValue.dismiss()
+            })
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
